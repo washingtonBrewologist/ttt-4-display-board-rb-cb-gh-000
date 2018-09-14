fca285eff2595ba236b8673e49da4147d19d9307
Nick Williams <nwilliams7539@student.whatcom.edu>
2018-09-14 00:41:46 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts "   |   |   "
+  puts " #{board[0]}  |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-  puts "X#{board[0]}"
 end
 
-board = [" "," "," "," "," "," "," "," "," "," ",]
+
+board = ["X"," "," "," "," "," "," "," "," "," ",]
 display_board(board)
